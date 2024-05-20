66b29fc660928a017843945be934587865e41a02
Javi <javier.thompson.idd@gmail.com>
2024-05-20 17:18:54 -0400

    about page completed + six-years-ago post

--- src/posts/six-years-ago.md
+++ src/posts/six-years-ago.md
@@ -1,12 +1,14 @@
 ---
 title: Transformación digital y buzzwords empresariales
-description: A primer on building a data store on Clickhouse.
+description: Una breve provocación respecto de la Transformación Digital.
 date: '2018-2-13'
 categories:
-  - clickhouse
-  - datawarehousing
+  - transformacion-digital
+  - hola
 published: true
 ---
-He leido excesivas veces la misma idea (los flujos de revenue vendran de analitica) y otras asociadas, como que la IA revoluciona los negocios, que los datos cambian las decisiones, que el Segmento de Uno y otras. Incluida esta la Transformacion Digital y otras buzzwords. Pero, la verdad, no he encontrado un estudio, white paper, fuente o consultora que se plantee la pregunta mas obvia: por que los ingresos vendran de la analitica y lo digital? Que es lo que realmente cambiara?
-La tencologia ha mejorado procesos y ha especializado a las personas, es innegable. La gente habita mas en dispositivos moviles que en tiendas fisicas, tambien es un hecho. Pero el mal cliente sigue siendo mal cliente tanto en su smartphone como en un call center como en el pasillo 3. Me siguen?
+He leido excesivas veces la misma idea (los flujos de revenue que vendrán de capacidades analíticas) y otras asociadas, como que la IA revoluciona los negocios, que los datos cambian las decisiones, que el Segmento de Uno y otras. Incluida esta la Transformaci+on Digital y otras _buzzwords_. Pero, la verdad, no he encontrado un estudio, _white paper_, fuente o consultora que se plantee la pregunta que me parece más obvia: por qué los ingresos vendrán de la analítica y lo digital? Qué es lo que realmente cambiará en el ambiente industrial?
+
+La tecnología ha mejorado procesos y ha especializado a las personas, es innegable. La gente habita más en dispositivos móviles que en tiendas físicas, también es un hecho. Pero el mal cliente sigue siendo mal cliente tanto en su smartphone como en un call center como en el pasillo 3. Me siguen?
+
 Alguien quiere aportar en este debate?
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,135 @@
+<script>
+    import mugshot from "$lib/assets/imgs/mugshot.jpg";
+</script>
+
+<div class="about-me">
+    <div class="three-columns">
+        <div class="col">
+            <h1>Yo Soy</h1>
+            <br>
+            <header>
+                Hello, friend.
+            </header>
+            <br>
+            <p>
+                Soy Javi Thompson. Un entusiasta tecnologista, con mucha experiencia en cosas bien diversas. En más de 12 años de trabajo, y más de 20 experimentando con tecnologías, me he formado como un <i>resolvedor de problemas</i>. No sólo como una persona que <i>resuelve</i>, sino también como alguien que analiza, discute, acuerda y propone. Que, honest, me parece un mínimo exigible hoy por hoy.
+            </p>
+            <p>
+                Trabajo en el espacio de los datos, más cercano y asociado a la ingeniería de datos. He tenido experiencias como analista, visualizador y científico, y me mantengo al día en el estado del arte leyendo un chingo de cosas 😅.
+            </p>
+            <p>
+                Me defino como un <i>tremendo nerd</i>, y por eso mismo muchas de las cosas que hago están asociadas a la tecnología (celus, compus, et al). Estudio mucho, pruebo muchas cosas, y juego mucho también. Y como no sólo de código vive el hombre, también hago otras cosas: <a href="https://www.instagram.com/raspado_de_olla" target="_blank">escribo poesía</a>, <a href="https://www.instagram.com/la.thompsoneria" target="_blank">tomo fotos</a>, y <a href="https://open.spotify.com/user/jtapiath/playlists" target="_blank">disfruto de mucha música</a>.
+            </p>
+            <br>
+            <img src={mugshot} alt="Javi Thompson 😌" />
+            <br><br>
+            <footer>
+                Speak, <i>mellon</i>, and enter.
+            </footer>
+        </div>
+        <div class="col">
+            <h1>Profesional de los datos</h1>
+            <br>
+            <header>
+                Un poco de mi trayectoria profesional:
+            </header>
+            <br>
+            <div class="main-list">
+                <ul>
+                    <li>Soy ingeniero civil industrial, con un minor en <i>Formación General</i> (i.e., un poco de todo), y una diplomatura en <b>innovación</b>.</li>
+                    <li>Partí trabajando como analista de mercados e industrias. Tuve experiencias en ello en EFT Group (now EVERTEC) y en Liberty Seguros.</li>
+                    <li>Tuve un breve paso como PMO, mirando las metodologías del PMI(tm).</li>
+                    <li>Me formé en datos en Coursera, edX, Udacity, Udemy, y otras plataformas de MOOCs.</li>
+                    <li>Mi primera experiencia como persona de datos fue de <b>analista de datos</b>, en Liberty Seguros.</li>
+                    <li>Emprendí el camino de la ingeniería de datos en Evalueserve, como consultor (tipo <i>staff augmentation</i>) en una institución financiera norteamericana.</li>
+                    <div class="secondary-list">
+                        <p>No les puedo decir cual, pero en esa época era la institución con mayor portafolio de créditos hipotecarios: <b><i>US$1.3 billones</i></b> en colocaciones.</p>
+                    </div>
+                    <li>He trabajado como <b>analista de datos</b>, <b>ingeniero de datos</b>, <b>ingeniero de visualización</b>, y un poco de <i>ciencia de datos</i>, <i>DevOps</i> y <i>DataOps</i>, y <b><i>arquitectura y preventa de soluciones</i></b>.</li>
+                    <li>He liderado células de desarrollo, equipos de proyecto, equipos de trabajo y áreas. Más recientemente, fui Líder de Equipo en Xepelin, una FinTech chilena.</li>
+                </ul>
+            </div>
+            <br><br>
+            <footer>
+                Not all those who wander, are lost. 
+            </footer>
+        </div>
+        <div class="col">
+            <h1>Mis proyectos</h1>
+            <br>
+            <header>
+                Algunos proyectos libres, la mayoría FOSS:
+            </header>
+            <br>
+            <div class="main-list">
+                <ol>
+                    <li><a href="https://github.com/darkestAbed/dufur" target="_blank"><b>Dufur</b></a>: una automatización que revisa las vacantes en clases de baile de una conocida academia de Santiago, Chile</li>
+                    <li><a href="https://github.com/DarkestAbed/vikareta" target="_blank"><b>Vikareta</b></a>: una solución <i>ERP-like</i> para PYMEs</li>
+                    <li><a href="https://github.com/DarkestAbed/de-journey" target="_blank"><b>The Data Engineer's Journey</b></a>: un compendio de primeros pasos para entrar en el mundo de la ingeniería de datos</li>
+                    <li><a href="https://github.com/DarkestAbed/repostore" target="_blank"><b>Repostore</b></a>: una solución <i>local-first</i> para guardar, listar y buscar tus repositorios de GitHub favoritos</li>
+                    <li><a href="https://github.com/DarkestAbed/de-challenge" target="_blank"><b>de-challenge</b></a>: un pequeño desafío para entender las capacidades de un candidato a ingeniero de datos</li>
+                    <li><a href="https://github.com/DarkestAbed/misderechos" target="_blank"><b>Mis Derechos</b></a>: un generador de reservas de derechos para la firma de finiquitos, de acuerdo a la ley laboral en Chile</li>
+                    <li><a href="https://github.com/jtapiath-cl/political-code" target="_blank"><b>Political Code</b></a>: análisis de texto y sentimiento del sistema de partidos políticos chileno</li>
+                    <li><a href="https://github.com/jtapiath-cl/historia_elecciones_chile" target="_blank"><b>Historia de las Elecciones en Chile</b></a>: análisis electoral cuantitativo para Chile</li>
+                    <li><b><a href="https://github.com/jtapiath-cl/analisis_covid" target="_blank">COVID analytics</a> & <a href="https://github.com/jtapiath-cl/covid_app" target="_blank">COVID app</a></b>: análisis, y la subsecuente productización, de la data gubernamental disponible respecto de la pandemia COVID-19</li>
+                    <li><a href="https://github.com/jtapiath-cl/distribucion_votos" target="_blank"><b>Distribución de votos</b></a>: análisis descriptivo de la distribución de votos en Chile, conducente (hopefully) a un análisis predictivo</li>
+                    <li><a href="https://github.com/jtapiath-cl/text_mining-entrevistas_pinera" target="_blank"><b>Text mining: Sebastián Piñera</b></a>: análisis de texto de algunos <i>corpus</i> compuesto por entrevistas al ex presidente chileno Sebastián Piñera</li>
+                </ol>
+            </div>
+            <br><br>
+            <footer>
+                Even the smallest person can change the course of the future.
+            </footer>
+        </div>
+    </div>
+</div>
+
+<style>
+    .about-me {
+        text-align: inherit;
+    }
+
+    .three-columns {
+        display: flex;
+        flex-wrap: nowrap;
+        width: 100%;
+        align-items: center top;
+        justify-content: center;
+        column-gap: 1rem;
+    }
+
+    .col {
+        width: 95%;
+        column-gap: 1rem;
+    }
+
+    .col > :is(h1) {
+        text-align: center;
+        text-overflow: clip;
+    }
+
+    .three-columns > .col {
+        width: calc(90% / 3);
+    }
+
+    .col > .main-list {
+        margin-left: 1rem;
+    }
+
+    .secondary-list > p {
+        text-indent: 0%;
+        margin-left: 1.5rem;
+    }
+
+    .main-list > :is(ul) {
+        list-style-type: '🪧';
+        margin-left: 2rem;
+        text-indent: 2%;
+    }
+
+    .main-list > :is(ol) {
+        list-style-type: '⚙️';
+        margin-left: 2rem;
+        text-indent: 2%;
+    }
+</style>
